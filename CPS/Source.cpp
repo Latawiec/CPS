e08390703f2452d8b79bf47859e8ab87223678f3
Latawiec <lmarzec08@gmail.com>
2018-03-18 22:18:31 +0100

    Defined operations.

--- CPS/Source.cpp
+++ CPS/Source.cpp
@@ -6,9 +6,15 @@
 
 #include "Complex.h"
 #include "Number.h"
-#include "Linear.h"
+
 #include "Sum.h"
+#include "Divide.h"
+#include "Multiply.h"
+#include "Subtract.h"
+
 #include "Sampler.h"
+
+#include "Linear.h"
 #include "Sinus.h"
 #include "SinusFlat.h"
 #include "SinusPositive.h"
@@ -26,10 +32,15 @@ void PutToFile(const Base::Data& aOutput)
 	auto now = std::chrono::system_clock::now();
 	auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
+	std::random_device generator{};
+	std::uniform_int_distribution<int> distribution(-10000, 10000);
+	int value = distribution(generator);
+
 	std::stringstream ss;
 	ss << "../QuickDisplay/data/";
 	ss << "data_";
 	ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d_%OH-%OM-%OS");
+	ss << "_" << value;
 	ss << ".json";
 
 	fstream file;
@@ -40,6 +51,7 @@ void PutToFile(const Base::Data& aOutput)
 		file << aOutput << endl;
 		file << "\"type\": \"scatter\"," << '\n';
 		file << "\"name\": \"" << std::put_time(std::localtime(&in_time_t), "%Y - %m - %d_%OS") << "\"\n";
+		ss << "_" << value;
 		file << '}';
 
 		file.close();
@@ -52,21 +64,28 @@ void PutToFile(const Base::Data& aOutput)
 
 int main()
 {
-	Sum sumOP{};
-	Sin sinOp(Sampler(0, 0.1, 10));
-	SinFlat sinFlat(Sampler(0, 0.1, 10));
-	SinPositive sinPositive(Sampler(0, 0.1, 10));
-	Linear line(Sampler(0, 0.1, 10), 0.0, 5.0);
-	Square square(Sampler(0, 0.01, 10), 0.4, 3);
-	Triangle triangle(Sampler(0, 0.01, 10), 0.4, 3);
-	Step step(Sampler(0, 0.01, 10), 5);
-	RandUniform rUniform(Sampler(0, 0.01, 10), 5);
-	RandNormal rNormal(Sampler(0, 0.01, 10));
+	Sum				sumOP{};
+	Divide			divOP{};
+	Multiply		mulOP{};
+	Subtract		subOP{};
 
-	auto XD = sumOP.AddInput(sinOp.Get())
-				   .AddInput(sinPositive.Get()).Execute().GetOutput();
+	Sin				sinOp(Sampler(0, 0.1, 10));
+	SinFlat			sinFlat(Sampler(0, 0.1, 10));
+	SinPositive		sinPositive(Sampler(0, 0.1, 10));
+	Linear			line(Sampler(0, 0.1, 10), 0.0, -1.0);
+	Square			square(Sampler(0, 0.01, 10), 0.4, 3);
+	Triangle		triangle(Sampler(0, 0.01, 10), 0.4, 3);
+	Step			step(Sampler(0, 0.01, 10), 5);
+	RandUniform		rUniform(Sampler(0, 0.01, 10), 5);
+	RandNormal		rNormal(Sampler(0, 0.01, 10));
 
-	PutToFile(rUniform.Get());
+	sumOP.AddInput(sinFlat.Get()).AddInput(sinPositive.Get()).Execute();
+
+	PutToFile(sumOP.GetOutput());
+
+	mulOP.AddInput(sumOP.GetOutput()).AddInput(line.Get()).Execute();
+
+	PutToFile(mulOP.GetOutput());
 
 	Number comp(1, 2);
 	Number comp1(3, 3);
