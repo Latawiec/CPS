a5d716133a872374a92f9f58f4ef070e0d58eb61
Latawiec <lmarzec08@gmail.com>
2018-05-06 19:49:08 +0200

    asdf

--- CPS/Source.cpp
+++ CPS/Source.cpp
@@ -57,7 +57,7 @@ int main()
 	Multiply		mulOP{};
 	Multiply		mulOP1{};
 	Subtract		subOP{};
-	//FromFile		fileRead("../QuickDisplay/data/dupa.json");
+	FromFile		fileRead("../QuickDisplay/data/dupa.json");
 	Average			avg{};
 	AverageMod		avgMod{};
 	Power			pwr{};
@@ -70,7 +70,7 @@ int main()
 	Sin				sinOp(a, 1, 2.0);
 	SinFlat			sinFlat(a, 2.0);
 	SinPositive		sinPositive(a, 2.0);
-	Linear			line(a, 0.0, 5.0);
+	Linear			line(a, 0.0, 0.0);
 	Linear			minus(a, 0.0, -1.0);
 	Square			square(a, 0.4, 3);
 	Triangle		triangle(a, 0.4, 3);
@@ -139,7 +139,7 @@ int main()
 	fileOP.AddInput(psnr).Execute();
 
 	//fileRead.Generate();
-
+/*
 	sinOp.Generate();
 	fileOP.AddInput(sinOp);
 	fileOP.Execute();
@@ -227,7 +227,7 @@ int main()
 		  .AddInput(sinFlat).Execute();
 
 	fileOP.AddInput(sumOP1);
-	fileOP.Execute();
+	fileOP.Execute();*/
 	
 	Number comp(1, 2);
 	Number comp1(3, 3);
